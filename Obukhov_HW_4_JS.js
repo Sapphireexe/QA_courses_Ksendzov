eaed5ef010f4e04a2aa1e665df70756f36c8d6e9
Anatoly Obukhov <sapphire.exe@gmail.com>
2022-05-02 00:30:13 +0300

    more explanations

--- Obukhov_HW_4_JS.js
+++ Obukhov_HW_4_JS.js
@@ -12,8 +12,8 @@
 //  8. Должна быть хотя бы одна @
 //  9. Строка не должна быть пустой
 
-//let validstr = prompt('Input from 5 to 64 symbols. Should include at least one uppercase letter, one number and one @');
-let validStr = 'Input from 5 to 64 symbols. Should include at least one uppercase letter, one number and one @';
+//let validstr = prompt('Input from 5 to 64 symbols. Should include at least one uppercase letter, one number and one @'); // for input from prompt window
+let validStr = 'Input from 5 to 64 symbols. Should include at least one uppercase letter, one number and one @'; // for input from code
 
 console.log(vLength(validStr));
 
